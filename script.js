7f1298612d115a49517ed99d70a4bbf8f2dc36e2
Techsations <akandedamilare.t@gmail.com>
2023-04-05 20:53:10 +0100

    I added the Js function for the signin form

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('container-2').style.display = 'none'
+function showIn() {
+    document.getElementById('container-1').style.display = 'none'
+    document.getElementById('container-2').style.display = 'block'
+}
+
+function showUp() {
+    document.getElementById('container-1').style.display = 'block'
+    document.getElementById('container-2').style.display = 'none'
+}
