3c155766909d2cbcd9364c806c85cadc47792f49
Techsations <akandedamilare.t@gmail.com>
2023-04-11 17:31:03 +0100

    I added a submit button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.getElementById('container-2').style.display = 'none'
+let timeout;
+
+
 function showIn() {
-    document.getElementById('container-1').style.display = 'none'
-    document.getElementById('container-2').style.display = 'block'
+    setTimeout(() => {
+        document.getElementById('container-1').style.display = 'none';
+        document.getElementById('container-2').style.display = 'block';
+    }, 5000);
 }
 
+
+
 function showUp() {
-    document.getElementById('container-1').style.display = 'block'
-    document.getElementById('container-2').style.display = 'none'
+   setTimeout(() => {
+    document.getElementById('container-1').style.display = 'block';
+    document.getElementById('container-2').style.display = 'none';
+   }, 5000);
 }
+
