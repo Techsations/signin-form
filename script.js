ece38a4b6218e4347183297102ccfd5325905d1b
Techsations <akandedamilare.t@gmail.com>
2023-04-12 16:37:46 +0100

    I updated the signup page functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-document.getElementById("loader").style.display = 'none'
-document.getElementById('container-2').style.display = 'none'
+let loader1 = document.getElementById("loader1");
+let loader = document.getElementById("loader");
+document.getElementById('container-2').style.display = 'none';
 let timeout;
 
 
@@ -19,26 +20,52 @@ function showUp() {
     document.getElementById('container-2').style.display = 'none';
    }, 3000);
 }
-let storage = []
-storage = JSON.parse(localStorage.getItem("user"))
+
 let email = document.getElementById("email");
 let username = document.getElementById("username");
 let password = document.getElementById("password");
+let signUpMessage = document.getElementById("signup-msg");
+
+loader1.style.display = 'none';
+loader.style.display = 'none';
 
 
 function submit() {
-    let data = {
-        email: email.value,
-        username: username.value,
-        password: password.value
-    }
-    storage.push(data)
-    localStorage.setItem("user", JSON.stringify(storage))
-    setTimeout(() => {
-        document.getElementById("loader").style.display = 'block'
-    }, 3000);
-    document.getElementById('container-1').style.display = 'none';
-    document.getElementById('container-2').style.display = 'block';
+    if (
+        password.value != "" &&
+        username.value != "" &&
+        email.value != ""
+      ) 
+        {
+            data = {
+                email: email.value,
+                username: username.value,
+                password: password.value,
+            };
+
+            let storage = JSON.parse(localStorage.getItem("user")) || [];
+            storage.push(data)
+            localStorage.setItem("user", JSON.stringify(storage))
+            signUpMessage.innerHTML = `
+            <p id="success-msg">Sign up successful!!!</p>
+            `;
+            loader1.style.display = 'block';
+
+            setTimeout(() => {
+                document.getElementById('container-1').style.display = 'none';
+                document.getElementById('container-2').style.display = 'block';
+                loading.style.display = "none";
+                signUpMessage.style.display = "none";
+            }, 3000);
+
+            password.value != "" &&
+            username.value != "" &&
+            email.value != ""
+        }else {
+            signUpMessage.innerHTML = `
+            <p id="failed-msg">Sign up failed!!!</p>
+            `;
+        }
 
 
     
