66bcba8b001e1b132a22b95bf52f9d05d04aaef2
Techsations <akandedamilare.t@gmail.com>
2023-04-17 17:23:44 +0100

    I add a new signin folder

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.getElementById("container-2").style.display = 'none';
+let loading = document.getElementById("loader");
+let email = document.getElementById("email");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let signUpMsg = document.getElementById("signup-msg");
+loading.style.display = "none";
 
 
 function logIn() {
   document.getElementById("container-1").style.display = 'none';
-  document.getElementById("container-2").style.display = 'block';
+  document.getElementById("container-2").style.display = 'block'; 
 }
 
 function signUp() {
