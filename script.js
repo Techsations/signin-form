ecf9c2b7c4883c68124d0ce5a0da1a9b874310fd
Techsations <akandedamilare.t@gmail.com>
2023-04-17 18:20:08 +0100

    I separated the signup and login pages

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("container-2").style.display = 'none';
 let loading = document.getElementById("loader");
 let email = document.getElementById("email");
 let username = document.getElementById("username");
@@ -7,12 +6,46 @@ let signUpMsg = document.getElementById("signup-msg");
 loading.style.display = "none";
 
 
-function logIn() {
-  document.getElementById("container-1").style.display = 'none';
-  document.getElementById("container-2").style.display = 'block'; 
+function logInPage() {
+  window.location.href = "signin.html";
 }
 
-function signUp() {
-  document.getElementById("container-1").style.display = 'block';
-  document.getElementById("container-2").style.display = 'none';
-}
+let storage = JSON.parse(localStorage.getItem("allUsers"));
+
+function submit(ev) {
+    ev.preventDefault();
+
+    if (username.value === "" || email.value === "" || password.value === "") {
+        alert("Please fill all the neccessary details")
+    } else if (password.value.length < 8) {
+        alert("Password must be at least 8 characters long")
+    } else {
+        let users = {
+            username: username.value,
+            email: email.value,
+            password: password.value,
+        }
+        if (storage === null) {
+          storage = [];
+          storage.push(users);
+          localStorage.setItem("allUsers", JSON.stringify(storage));
+      } else {
+          storage.push(users);
+          localStorage.setItem("allUsers", JSON.stringify(storage));
+      }
+      signUpMsg.innerHTML = `
+          <p id="success-msg">Sign up successful!!!</p>
+          `
+      loading.style.display = "block";
+
+      setTimeout(() => {
+        window.location.href = "signin.html";
+        loading.style.display = "none";
+        signUpMsg.style.display = 'none';
+    }, 3000);
+
+    username.value = "";
+    email.value = "";
+    password.value = "";
+      }
+    }
---
+++ signIn/signin.js
@@ -0,0 +1,3 @@
+let loading = document.getElementById("loader");
+let userLogin = document.getElementById('userLogin');
+let passLogin = document.getElementById('passLogin');
