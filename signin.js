d4e813dd26554fddc4d866f9e9cc1f1868f943b5
Techsations <akandedamilare.t@gmail.com>
2023-04-18 10:46:06 +0100

    I started writing the logIn function

--- signin.js
+++ signin.js
@@ -10,3 +10,17 @@ function signUpPage() {
 
 let storage = JSON.parse(localStorage.getItem("allUsers"));
 console.log(storage);
+
+function logIn() {
+    // ev.preventDefault();
+    let authorizedUser = storage.find((el)=> el.username == userLogin.value && el.password == passLogin.value);
+    console.log(authorizedUser);
+    loading.style.display = "block";
+    if (!authorizedUser) {
+        setTimeout(() => {
+            loading.style.display = "none";
+        }, 2900);
+        setTimeout(() => {
+            alert("User Information Incorrect");
+        }, 3000);
+    }
